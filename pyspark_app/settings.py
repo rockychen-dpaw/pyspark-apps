6b1aedc234663a9ab7154a8d5f2bff8e19bacc8f
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2023-03-23 16:09:29 +0800

    Load the spark conf from json file

--- pyspark_app/settings.py
+++ pyspark_app/settings.py
@@ -2,6 +2,7 @@ import os
 import logging.config
 import pytz
 import logging
+import json
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -13,16 +14,12 @@ if LOGGING_CONF:
 
 logger = logging.getLogger("pyspark_app.app.nginxaccesslog")
 
-SPARK_MASTER = "spark://master.spark.dbca.wa.gov.au:7077"
-DEPLOY_MODE =  "client"
-SPARK_CONF = {
-    "spark.master":"spark://master.spark.dbca.wa.gov.au:7077",
-    "spark.submit.deployMode":"client",
-    "spark.driver.cores":1,
-    "spark.driver.memory":"100m",
-    "spark.executor.memory":"450m",
-    "spark.executor.pyspark.memory":"800m"
-}
+try:
+    with open(os.environ.get("SPARK_CONF")) as f:
+        SPARK_CONF = json.loads(r.read())
+except Exception as ex:
+    raise Exception("Failed to load spark configuration.")
+ 
 
 TIMEZONE = pytz.timezone("Australia/Perth")
 GEOIP_DATABASE_HOME=os.environ.get("GEOIP_DATABASE_HOME")
