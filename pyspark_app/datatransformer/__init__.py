ad6566a198814fe6a68a65426b00bee9906dfadf
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2023-10-17 11:13:26 +0800

    Change default value of the parameter 'return_id' to True

--- pyspark_app/datatransformer/__init__.py
+++ pyspark_app/datatransformer/__init__.py
@@ -33,7 +33,7 @@ def clean():
     datetimes.clean()
     enums.clean()
 
-def transform(f_name,val,databaseurl=None,columnid=None,context=None,record=None,columnname=None,return_id=False,**kwargs):
+def transform(f_name,val,databaseurl=None,columnid=None,context=None,record=None,columnname=None,return_id=True,**kwargs):
     _func,f = _transformers[f_name]
     return _func(f,val,databaseurl=databaseurl,columnid=columnid,context=context,record=record,columnname=columnname,**kwargs)
 
