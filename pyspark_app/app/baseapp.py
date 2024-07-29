bc3c4e9a8a5aca879370fe21a6ca30011fc2c164
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2024-07-29 16:30:28 +0800

    Decode the parameter of the rawdatacondition

--- pyspark_app/app/baseapp.py
+++ pyspark_app/app/baseapp.py
@@ -57,7 +57,6 @@ def rawdatacondition_factory(column_map,rawdataconditions):
             funcs.append((operation.get_func(col[EXECUTOR_DTYPE],cond[1]),lambda row:row[col[EXECUTOR_COLUMNINDEX]],cond[2]))
     def _func(row):
         for func in funcs:
-            logger.error("{}({},{})".format(func[0],func[1](row),func[2]))
             if not func[0](func[1](row),func[2]):
                 return False
 
@@ -2455,6 +2454,10 @@ class DatasetAppReportDriver(DatasetAppDownloadDriver):
                 self.distinct_columns = None
                 self.distinct_colname = None
                 self.report_type = NoneReportType
+                if self.report_rawdataconditions:
+                    for cond in self.report_rawdataconditions:
+                        cond[2] = cond[2].strip() if cond[2] else cond[2]
+                        cond[2] = json.loads(cond[2])
             else:
                 #rawdata conditions doesn't support for statistics report
                 self.report_rawdataconditions = None
